d9a0ef8d2b11b78212404a486d25d42cbf43923d
Paul David Tan <cryxuz@gmail.com>
2023-12-05 03:03:08 +1300

    search bar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#38a6f5'
+      }
+    },
   },
   plugins: [],
 }
