30c86c73031b72f270b2e09a14e35f39d7e5d257
Paul David Tan <cryxuz@gmail.com>
2023-12-04 19:32:05 +1300

    started nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,20 @@ import './App.css'
 function App() {
 
   return (
-    <div></div>
+    <div>
+      <header className='p-4'>
+        <a href="" className="flex items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>
+        <span className='font-bold text-xl'>(change logo)Nestify</span>
+        </a>
+      </header>
+      {/* search */}
+      <div>
+
+      </div>
+    </div>
   )
 }
 
