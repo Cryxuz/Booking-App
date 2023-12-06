78ab364c99492deb22820bf40f541573b00f1f9c
Paul David Tan <cryxuz@gmail.com>
2023-12-06 13:56:42 +1300

    register link

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const LoginPage = () => {
   return (
     <div className="mt-4 grow flex items-center justify-around">
@@ -7,6 +9,11 @@ const LoginPage = () => {
           <input type="email" placeholder={'example@example.com'} name="email" id="email" />
           <input type="password" placeholder="password" />
           <button className="primary">Login</button>
+          <div className="text-center py-2 text-gray-500">
+            Don&apos;t have an account yet?
+            <br />
+            <Link className="text-black underline" to={'/register'}>Register Now</Link>
+          </div>
         </form>
       </div>
     </div>
