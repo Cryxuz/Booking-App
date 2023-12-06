2840648496168282791e5111c93292bd337da425
Paul David Tan <cryxuz@gmail.com>
2023-12-06 14:33:02 +1300

    get request success client to server connected

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import axios from 'axios'
 
 const RegisterPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const registerUser = (e) => {
+    e.preventDefault()
+    axios.get('/test')
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto" action="">
-          <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder={'example@example.com'} name="email" id="email" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input 
+                type="text" 
+                placeholder="John Doe"
+                value={name} 
+                onChange={e => setName(e.target.value)} />
+          <input 
+                type="email" 
+                placeholder={'example@example.com'} 
+                value={email} 
+                onChange={e => setEmail(e.target.value)}
+                />
+          <input 
+                type="password" 
+                placeholder="password"
+                value={password} 
+                onChange={e => setPassword(e.target.value)}
+                />
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already a member?
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
 const PORT = 3000
+app.use(cors({
+  credentials: true,
+}))
 
 app.get('/test', (req,res) => {
   res.json('testok')
