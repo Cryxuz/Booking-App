94f1a4b4e71f45f2d975b679181871e49c0c903b
Paul David Tan <cryxuz@gmail.com>
2023-12-07 18:19:24 +1300

    register success, errors on console after register neex fix

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ import cors from 'cors'
 import * as dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import User from './models/User.js'
+import bcrypt from 'bcryptjs'
 
 const app = express()
+dotenv.config()
+
+const bcryptSalt = bcrypt.genSaltSync(10)
 app.use(express.json())
 const PORT = 3000
 app.use(cors())
-dotenv.config()
+
 
 mongoose.connect(process.env.MONGO_URL)
 
@@ -16,14 +20,14 @@ app.get('/test', (req,res) => {
   res.json('testok')
 })
 
-app.post('/register', (req,res) => {
+app.post('/register', async (req,res) => {
   const {name, email, password} = req.body
-  User.create({
+  const user = await User.create({
     name,
     email,
-    password,
+    password: bcrypt.hashSync(password, bcryptSalt),
   })
-  res.json(name, email, password)
+  res.json(user)
 })
 
 app.listen(PORT)
