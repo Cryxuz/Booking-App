179cc5bb6844487815372643adb4a7481e072600
Paul David Tan <cryxuz@gmail.com>
2023-12-07 18:02:33 +1300

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import * as dotenv from 'dotenv'
 
 const app = express()
 app.use(express.json())
 const PORT = 3000
 app.use(cors())
+dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL)
 
