c2750dc755cf2c8699f3695d75771010af908270
Paul David Tan <cryxuz@gmail.com>
2023-12-11 11:26:43 +1300

    Place schema

---
+++ server/models/place.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const PlaceSchema = new mongoose.Schema({
+  title: String,
+  address: String,
+  photos: [String],
+  description: String,
+  perks: [String],
+  extraInfo: String,
+  checkIn: Number,
+  checkOut: Number,
+  maxGuests: Number
+})
