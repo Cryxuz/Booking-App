c565408277fa81ff2c3d1e51c92c2228577603fa
Paul David Tan <cryxuz@gmail.com>
2023-12-10 13:09:11 +1300

    navigate to login if user is not logged in

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,5 +1,14 @@
+import { useContext } from "react"
+import { UserContext } from "../UserContext.jsx"
+import { Navigate } from "react-router-dom"
+
 export const AccountPage = () => {
+  const {user} = useContext(UserContext)
+
+  if(!user) {
+    return <Navigate to={'/login'} />
+  }
   return (
-    <div>Account</div>
+    <div>Account for {user.name}</div>
   )
 }
