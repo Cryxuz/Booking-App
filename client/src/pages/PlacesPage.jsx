d9ff3f3d9050f34cf72254fff191b753f738ef60
Paul David Tan <cryxuz@gmail.com>
2023-12-13 21:42:29 +1300

    errors login failed

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -17,6 +17,9 @@ const PlacesPage = () => {
   const [checkOut, setCheckOut] = useState('')
   const [maxGuests, setMaxGuests] = useState(1)
 
+  function uploadPhoto(evt) {
+    
+  }
   function inputHeader(text) {
     return <h2 className='mt-4 text-2xl' htmlFor='title'>{text}</h2>;
   }
@@ -90,7 +93,7 @@ const PlacesPage = () => {
                 </div>
               ))}
               <label className='cursor-pointer flex gap-2 justify-center border bg-transparent rounded-2xl items-center p-2 text-2xl text-gray-600'>
-                <input type="file" className='hidden'/>
+                <input type="file" className='hidden' onChange={uploadPhoto}/>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
