a6172cf7b6bbb0b4c427237c9b485ba76ae342e4
Paul David Tan <cryxuz@gmail.com>
2023-12-14 23:40:06 +1300

    test upload file fix

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -20,7 +20,9 @@ const PlacesPage = () => {
   function uploadPhoto(evt) {
     const files = evt.target.files
     const data = new FormData()
-    data.set('photos', files)
+    for (const file of files) {
+      data.set('photos[]', files)
+    }
     axios.post('/upload', data, {
       headers: {'Content-type': 'multipart/form-data'}
     }).then( response => {
@@ -104,7 +106,7 @@ const PlacesPage = () => {
                 </div>
               ))}
               <label className='cursor-pointer flex gap-2 justify-center border bg-transparent rounded-2xl items-center p-2 text-2xl text-gray-600'>
-                <input type="file" className='hidden' onChange={uploadPhoto}/>
+                <input type="file" multiple className='hidden' onChange={uploadPhoto}/>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
