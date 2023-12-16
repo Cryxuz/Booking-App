f0a88c22e229d56b6da5a46ca5d678b519661c1f
Paul David Tan <cryxuz@gmail.com>
2023-12-16 20:16:39 +1300

    refactoring code

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,9 +1,10 @@
 import { useContext } from "react"
 import { UserContext } from "../UserContext.jsx"
-import { Link, Navigate, useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import axios from "axios"
 import { useState } from "react"
 import PlacesPage from "./PlacesPage.jsx"
+import { AccountNavigation } from "./AccountNavigation.jsx"
 
 export const AccountPage = () => {
   const {ready, user, setUser} = useContext(UserContext)
@@ -20,11 +21,9 @@ export const AccountPage = () => {
       await axios.post('/logout')
       setRedirect('/')
       setUser(null)
-
     } catch (err) {
       console.log(err)
-    }
-   
+    } 
   }
 
   if(!ready) {
@@ -35,43 +34,12 @@ export const AccountPage = () => {
     return <Navigate to={'/login'} />
   }
 
-  function linkClasses (type=null) {
-    let classes =  "inline-flex gap-2 py-2 px-6 rounded-full"
-    if (type === subpage) {
-      classes += ' bg-primary text-white rounded-full'
-    } else {
-      classes += ' bg-gray-300'
-    }
-    return classes
-  }
-  
   if(redirect) {
     return <Navigate to={redirect} />
   }
-
-
   return (
     <div>
-      <nav className="w-full flex mt-8 gap-2 justify-center mb-8">
-        <Link className={linkClasses('profile')} to={'/account'}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-          </svg>
-          My profile
-        </Link>
-        <Link className={linkClasses('bookings')} to={'/account/bookings'}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-          </svg>
-          My bookings
-        </Link>
-        <Link className={linkClasses('places')} to={'/account/places'}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-          </svg>
-          My accomodations
-        </Link>
-      </nav>
+      <AccountNavigation />
       {subpage === 'profile' && (
         <div className="text-center max-w-md mx-auto">
           Logged in as {user.name} ({user.email})
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,62 +1,12 @@
-import { Link, Navigate, useParams } from 'react-router-dom'
-import { useState } from 'react'
-import Perks from '../Perks'
-import PhotosUploader from '../PhotosUploader'
-import axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
+import PlacesFormPage from './PlacesFormPage'
+
 
 
 const PlacesPage = () => {
-  const [addedPhotos, setAddedPhotos] = useState([])
+
   const {action} = useParams()
-  const [title, setTitle] = useState('')
-  const [address, setAddress] = useState('')
-  const [description, setDescription] = useState('')
-  const [perks, setPerks] = useState([])
-  const [extraInfo, setExtraInfo] = useState('')
-  const [checkIn, setCheckIn] = useState('')
-  const [checkOut, setCheckOut] = useState('')
-  const [maxGuests, setMaxGuests] = useState(1)
-  const [redirect, setRedirect] = useState('')
-
-  const handlePhotosChange = (newPhotos) => {
-    setAddedPhotos(newPhotos);
-  };
   
-  function inputHeader(text) {
-    return <h2 className='mt-4 text-2xl' htmlFor='title'>{text}</h2>;
-  }
-  
-  function preInput(headerText) {
-    return (
-      <div>
-        {inputHeader(headerText)}
-      </div>
-    );
-  }
-  
-  async function addNewPlace (evt) {
-    evt.preventDefault()
-   
-   await axios.post('/places', {
-      title,
-      address,
-      addedPhotos,
-      description,
-      perks,
-      extraInfo,
-      checkIn,
-      checkOut,
-      maxGuests
-    },
-    {
-      withCredentials:true
-    })
-    setRedirect('/account/places')
-  }
-
-  if (redirect) {
-    return <Navigate to={redirect} />
-  }
   return (
     <div>
       {action !== 'new' && (
@@ -70,84 +20,7 @@ const PlacesPage = () => {
       </div>
       )}
       {action === 'new' && (
-        <div> 
-          <form onSubmit={addNewPlace}>
-            {/* title */}
-            {preInput('Title')}
-            <input 
-              value={title} 
-              onChange={evt => setTitle(evt.target.value)} 
-              id='title' 
-              type="text" 
-              placeholder='Title: My lovely apartment' 
-            /> 
-            {/* address */}
-            {preInput('Address')}
-            <input 
-              value={address} 
-              onChange={evt => setAddress(evt.target.value)} 
-              id='address' 
-              type="text" 
-              placeholder='Address: 123 Fake Street' 
-            /> 
-            {/* photos */}
-            {preInput('Photos')}
-            <PhotosUploader addedPhotos={addedPhotos} onChange={handlePhotosChange}/>
-            {/* description */}
-            {preInput('Description')}
-            <textarea 
-              value={description} 
-              onChange={evt => setDescription(evt.target.value)} 
-            />
-            {/* perks */}
-            {preInput('Perks')}
-            <div className='mt-2 grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
-              <Perks selected={perks} onChange={setPerks} />
-            </div>
-            {/* extra info */}
-            {preInput('Extra Info')}
-            <p className='text-sm text-gray-500'>House rules, etc.</p>
-            <textarea 
-              value={extraInfo} 
-              onChange={evt => setExtraInfo(evt.target.value)}
-              />
-            {/* check in & out */}
-            {preInput('Check in & out times')}
-            <div className='grid gap-2 sm:grid-cols-3'>
-              {/* check in */}
-              <div>
-                <h3 className='mt-2 -mb-1'>Check in time:</h3>
-                <input 
-                  value={checkIn} onChange={evt => setCheckIn(evt.target.value)}
-                  type="text" 
-                  placeholder='14:00'
-                />
-              </div>
-              {/* check out */}
-              <div>
-                <h3 className='mt-2 -mb-1'>Check out time:</h3>
-                <input
-                  value={checkOut} 
-                  onChange={evt => setCheckOut(evt.target.value)} 
-                  type="text" 
-                  placeholder='11:00'
-                />
-              </div>
-              {/* max guests */}
-              <div>
-                <h3>Max guests</h3>
-                <input 
-                  value={maxGuests} 
-                  onChange={evt => setMaxGuests(evt.target.value)}
-                  type="number" 
-                />
-              </div>
-            </div>  
-            <div className='flex justify-center'>
-              <button className='primary my-4 md:max-w-[50%] '>Save</button>
-            </div>        
-          </form>
-        </div>
+        <PlacesFormPage />
       )}
 
     </div>
