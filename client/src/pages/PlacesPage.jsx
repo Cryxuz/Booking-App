d6889c7b1b0eb5bf91c000278cfc474573315761
Paul David Tan <cryxuz@gmail.com>
2023-12-20 16:11:58 +1300

    img fix in accomodations

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -33,9 +33,9 @@ const PlacesPage = () => {
         {places.length > 0 &&
           places.map((place) => (
           <Link to={'/account/places/' + place._id} className='flex cursor-pointer bg-gray-100 p-4 rounded-2xl gap-4' key={place._id}>
-            <div className='w-32 h-32 bg-gray-300 grow shrink-0'>
+            <div className='flex w-32 h-32 bg-gray-300 grow shrink-0'>
               {place.photos.length > 0 && (
-                <img src={place.photos[0]} alt={place.title} />
+                <img className='object-cover' src={'http://localhost:3000/uploads/'+place.photos[0]} alt={place.title} />
               )}
             </div>
             <div className='grow-0 shrink'>
