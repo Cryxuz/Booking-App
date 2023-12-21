746f7bacfd488328c29098eba3bdf3f2cc22a31c
Paul David Tan <cryxuz@gmail.com>
2023-12-21 14:27:00 +1300

    title and address for index page, start of prices per accomodation

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -5,21 +5,22 @@ export default function IndexPage() {
   const [places, setPlaces] = useState([])
   useEffect(() => {
     axios.get('/places').then(response => {
-      setPlaces(response.data)
+      setPlaces([...response.data, ...response.data, ...response.data, ...response.data,])
     })
   }, [])
   console.log(places)
   return (
-       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8">
+       <div className="grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8">
         {places.length > 0 && places.map(place => (
           <div key={place.id}>
-            <div className="flex">
+            <div className="flex mb-2">
               {place.photos?.[0] && (
 // CHANGE LOCALHOST BEFORE DEPLOYING
                   <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:3000/uploads/' + place.photos?.[0]} alt={place.id} />
               )}
             </div>
-            {place.title}
+            <h2 className="text-md truncate leading-4">{place.title}</h2>
+            <h3 className="font-bold ">{place.address}</h3>
           </div>
         ))}
       </div>)
