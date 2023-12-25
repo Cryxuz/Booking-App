b1d14d7ec72760e4961aca326146e3bfb4605536
Paul David Tan <cryxuz@gmail.com>
2023-12-25 20:13:33 +1300

    styles singlePage

--- client/src/pages/SinglePage.jsx
+++ client/src/pages/SinglePage.jsx
@@ -26,7 +26,7 @@ const SinglePage = () => {
       <div className='absolute inset-0 bg-black text-white min-h-screen'>
         <div className='bg-black p-8 grid gap-4'>
           <div>
-            <h2 className='text-3xl'>Photos of {place.title}</h2>
+            <h2 className='text-3xl mr-48'>Photos of {place.title}</h2>
             <button onClick={() => setShowAllPhotos(false)} className='right-12 top-8 flex gap-1 py-2 px-4 rounded-xl fixed shadow shadow-black bg-white text-black'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
@@ -59,18 +59,18 @@ const SinglePage = () => {
         <div>{place.photos && place.photos.length > 0 && (
           <div>
             {/* // change localhost before deploying */}
-            <img onClick={() => setShowAllPhotos(true)}className='aspect-square object-cover' src={'http://localhost:3000/uploads/'+place.photos[0]} alt="" />
+            <img onClick={() => setShowAllPhotos(true)}className='cursor-pointer aspect-square object-cover' src={'http://localhost:3000/uploads/'+place.photos[0]} alt="" />
           </div>
         )}</div>
 
           <div className='grid'>
             <div>{place.photos && place.photos.length > 0 && (
 
-              <img onClick={() => setShowAllPhotos(true)}className='aspect-square object-cover' src={'http://localhost:3000/uploads/'+place.photos[1]} alt="" />
+              <img onClick={() => setShowAllPhotos(true)}className='cursor-pointer aspect-square object-cover' src={'http://localhost:3000/uploads/'+place.photos[1]} alt="" />
             )}</div>
               <div className='overflow-hidden'>
                 {place.photos && place.photos.length > 0 && (
-              <img onClick={() => setShowAllPhotos(true)}className='aspect-square object-cover relative top-2' src={'http://localhost:3000/uploads/'+place.photos[2]} alt="" />
+              <img onClick={() => setShowAllPhotos(true)}className='cursor-pointer aspect-square object-cover relative top-2' src={'http://localhost:3000/uploads/'+place.photos[2]} alt="" />
             )}</div>
           </div>
           <button onClick={() => setShowAllPhotos(true)} className='flex gap-2 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-xl shadow-md shadow-gray-500'>
