d7668b55b8f708c44fc0e08740de8a4ac9d2d630
Paul David Tan <cryxuz@gmail.com>
2023-12-30 17:57:52 +1300

    errors getting data fixed for bookings

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react'
 import AccountNavigation from './AccountNavigation'
+import axios from 'axios'
 
 const BookingsPage = () => {
+
+  const [bookings, setBookings] = useState([])
+
+  useEffect(() => {
+    axios.get('bookings', { withCredentials: true}).then(response => {
+      setBookings(response.data)
+    })
+  }, [])
+
   return (
     <div>
       <AccountNavigation />
       <div>
-        
+        {bookings.length > 0 && bookings.map((booking, index) => (
+          <div key={index}>
+            {booking.checkIn} -&gt; {booking.checkOut}
+          </div>
+        ))}
       </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -45,14 +45,26 @@ app.use(cors({
 // stored in .env file for security
 mongoose.connect(process.env.MONGO_URL)
 
-function getUserDataFromReq(req){
+function getUserDataFromReq(req) {
   return new Promise((resolve, reject) => {
-    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
-      if(err) throw err;
-      resolve(userData)
-    })
-  })
+    const token = req.cookies.token;
+    if (!token) {
+      // Handle the case where the token is missing
+      return reject(new Error('Token missing'));
+    }
+
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      if (err) {
+        // Handle the case where token verification fails
+        console.error('Token verification failed:', err.message);
+        return reject(err);
+      }
+
+      resolve(userData);
+    });
+  });
 }
+
 
 app.get('/test', (req,res) => {
   res.json('testok')
